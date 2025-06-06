420d69aaf88282db8d41a7bc124910cf25c5633b
Vincenzo Spinelli <contact@vincenzospinelli.it>
2025-06-06 17:54:24 +0200

    Add tailwind from npm

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./content.js",
+    "./config/config.html",
+    "./assets/**/*.html",
+    "./assets/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [],
+};
